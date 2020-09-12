697e88b340c30ccca8c2814a4e6f45a4a97940d7
Kurt King <kurtaking@gmail.com>
2020-09-12 16:07:45 -0500

    Added default message if no ingredients
    - Added a check to see if the ingredients array is empty. This will now show a "Please add ingredients" note to the user. This finishes the initial Burger functionality until we add button controls for the ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from './Burger.module.css'
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
         return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
-      });
-    });
+      })
+
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+
+  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
 
   return(
 
