e11085211fc2a4ccecb4e9ce7d9f0b584b72a8bb
Kurt King <kurtaking@gmail.com>
2020-09-12 16:00:12 -0500

    Dynamically Creating Burger Ingredients
    We are now dynamically creating the BurgerIngredient component based
    on the hardcoded state in the BurgerBuilder container

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,15 +2,22 @@ import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from './Burger.module.css'
 
-const burger = () => {
- return(
-   <div className={classes.Burger}>
-     <BurgerIngredient type="bread-top" />
-     <BurgerIngredient type="cheese" />
-     <BurgerIngredient type="meat" />
-     <BurgerIngredient type="bread-bottom" />
-   </div>
- );
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+        return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+      });
+    });
+
+  return(
+
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+);
 }
 
 export default burger;
